144fc82b512af6c503a56f0345d05496353605d6
armanruet <armanruet@gmail.com>
2026-05-12 21:42:54 +0000

    Task 12 done with testcase delete

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -5,7 +5,3 @@
 def np_elementwise(mat1, mat2):
     """defining the function"""
     return (mat1+mat2, mat1-mat2, mat1*mat2, mat1/mat2)
-
-
-mat1 = np.array([[11, 22, 33], [44, 55, 66]])
-mat2 = np.array([[1, 2, 3], [4, 5, 6]])
