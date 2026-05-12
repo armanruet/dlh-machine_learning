a4290a8775cfc8678c11e912515500854c09f613
armanruet <armanruet@gmail.com>
2026-05-12 21:09:26 +0000

    using only .shape

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """calculates the shape of a numpy.ndarray"""
-import numpy as np
 
 
 def np_shape(matrix):
     """defining function"""
-    return np.shape(matrix)
+    return matrix.shape
