25875466af27db29c25444237e88b0715a5ef801
armanruet <armanruet@gmail.com>
2026-05-13 23:34:32 +0000

    Task 7 done with None

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -4,7 +4,9 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """Defining the function"""
-    if axis == 0:
+    if axis == 0 and len(mat1[0]) == len(mat2[0]):
         return mat1 + mat2
-    if axis == 1:
+    if axis == 1 and len(mat1) == len(mat2):
         return [mat1[i] + mat2[i] for i in range(len(mat1))]
+    else:
+        return None
