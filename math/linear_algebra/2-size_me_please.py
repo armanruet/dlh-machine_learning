e6459c5807ab36fea62ebb26094c1e822c38eb18
armanruet <armanruet@gmail.com>
2026-05-16 13:29:15 +0000

    Task 2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+
 def matrix_shape(matrix):
-    if not isinstance(matrix, list):
-        return []
-    return [len(matrix)] + matrix_shape(matrix[0])
+    shape = []
+    x = matrix
+    while isinstance(x, list) and len(x) > 0:
+        shape.append(len(x))
+        x = x[0]   # go one level deeper
+    return shape
