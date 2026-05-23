9f3a09b6a09f582852978cdd40c45a439c899fe5
armanruet <armanruet@gmail.com>
2026-05-23 11:46:02 +0000

    Task 6 done with DB name

--- pipeline/databases/6-avg_temperatures.sql
+++ pipeline/databases/6-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- Create script that computes the average temp by city
-SELECT city, AVG(temperature) as avg_temp FROM hbtn_0c_0 GROUP BY city ORDER BY avg_temp DESC
+SELECT city, AVG(temperature) as avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC
