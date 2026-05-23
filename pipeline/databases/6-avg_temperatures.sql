3795bc6a86481f3a6ee0d38ee93a383b9562425c
armanruet <armanruet@gmail.com>
2026-05-23 12:27:59 +0000

    Fixing column name

--- pipeline/databases/6-avg_temperatures.sql
+++ pipeline/databases/6-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- Create script that computes the average temp by city
-SELECT city, AVG(temperature) as avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC
+SELECT city, AVG(value) as avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC
