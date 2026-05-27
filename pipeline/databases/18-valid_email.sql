9b03756a84ac8c3547dbe6a6623cba84ad80ab67
armanruet <armanruet@gmail.com>
2026-05-27 09:38:18 +0000

    Task 18 completed

--- pipeline/databases/18-valid_email.sql
+++ pipeline/databases/18-valid_email.sql
@@ -1,15 +1,6 @@
 --resetting email trigger
 DROP TRIGGER IF EXISTS reset_valid_email;
-
-DELIMITER $$
-
 CREATE TRIGGER reset_valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
-BEGIN
-    IF NEW.email != OLD.email THEN
-        SET NEW.valid_email = 0;
-    END IF;
-END$$
-
-DELIMITER ;
+SET NEW.valid_email = IF(NEW.email != OLD.email, 0, NEW.valid_email);
