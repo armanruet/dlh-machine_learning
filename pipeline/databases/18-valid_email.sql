64a04093d4dfbf5d2da1fd68ed4f2767072cc0ca
armanruet <armanruet@gmail.com>
2026-05-27 08:56:35 +0000

    Task 18 done

---
+++ pipeline/databases/18-valid_email.sql
@@ -0,0 +1,9 @@
+--resetting email trigger
+CREATE TRIGGER reset_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END;
