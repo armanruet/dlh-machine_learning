34110a95fa191636d15aa47a301d058d50c738b9
armanruet <armanruet@gmail.com>
2026-05-27 23:50:42 +0200

    task 32 done with comment

--- pipeline/databases/32-update_topics.py
+++ pipeline/databases/32-update_topics.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""Module that updates topics in a school document based on the name."""
+
 def update_topics(mongo_collection, name, topics):
     """Change all topics of a school document based on the name."""
     mongo_collection.update_many(
