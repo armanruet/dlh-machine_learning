83ae921eb73ea03291e3e04dc16bb6e987d7fe07
armanruet <armanruet@gmail.com>
2026-05-27 23:34:42 +0200

    Task 31 done

---
+++ pipeline/databases/31-insert_school.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+"""Function that inserts a new document in a collection"""
+def insert_school(mongo_collection, **kwargs):
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
