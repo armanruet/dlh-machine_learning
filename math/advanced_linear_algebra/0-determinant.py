08ab0f0c4e63cc81dde5d1a2dbee7efbaf937c77
armanruet <armanruet@gmail.com>
2026-05-28 17:11:40 +0200

    Task 0 typo fixed

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """Function to determine the Determinant"""
 
 
@@ -9,15 +8,14 @@ def determinant(matrix):
         return 1
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
-    if len(matrix) != len(matrix[0]):
+    if not all(len(row) == len(matrix) for row in matrix):
         raise ValueError("matrix must be a square matrix")
     if len(matrix) == 1:
         return matrix[0][0]
     if len(matrix) == 2:
-        return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
     total = 0
     for j in range(len(matrix)):
-        # Minor: delete row 0, col j
         minor = [[matrix[i][k] for k in range(len(matrix))
                   if k != j]
                  for i in range(1, len(matrix))]
