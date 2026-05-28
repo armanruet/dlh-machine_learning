8996c95e1cb2c12ba765852538613f2739c352c1
armanruet <armanruet@gmail.com>
2026-05-28 15:10:26 +0200

    Task 0 done

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
 
 """Function to determine the Determinant"""
+
+
 def determinant(matrix):
     """defining the function"""
     if matrix == [[]]:
         return 1
     if not matrix:
-        return TypeError ("matrix must be a list of lists")
-    if len(matrix)!= len(matrix[0]):
-        return ValueError ("matrix must be a square matrix")
-    if len(matrix)==2:
+        return TypeError("matrix must be a list of lists")
+    if len(matrix) != len(matrix[0]):
+        return ValueError("matrix must be a square matrix")
+    if len(matrix) == 1:
+        return matrix[0][0]
+    if len(matrix) == 2:
         return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
-mat0 = [[]]    
-mat5 = []
-mat6 = [[1, 2, 3], [4, 5, 6]]
-print(determinant(mat0))
-print(determinant(mat6))
-print(determinant(mat5))
+    total = 0
+    for j in range(len(matrix)):
+        # Minor: delete row 0, col j
+        minor = [[matrix[i][k] for k in range(len(matrix))
+                  if k != j]
+                 for i in range(1, len(matrix))]
+        sign = (-1) ** j
+        total += sign * matrix[0][j] * determinant(minor)
+    return total
