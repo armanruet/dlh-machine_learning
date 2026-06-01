e3d521422af1fe685a4081ad6a841ef992db1827
armanruet <armanruet@gmail.com>
2026-06-01 13:03:51 +0000

    Task 9 done

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ a function def summation_i_squared(n): that calculates"""
 
-
 def summation_i_squared(n):
+    """defining the function"""
     if not isinstance(n, (int, float)):
         return None
     else:
