065677af4f14ff3b33bf31e95d3af4cd11fc4e0a
armanruet <armanruet@gmail.com>
2026-06-03 14:24:28 +0000

    Task 10 done

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 """Derive happiness in oneself from a good day's work"""
+
+
 def poly_derivative(poly):
     """defining the function"""
-
+    poly_f = []
+    if not isinstance(poly, list):
+        return None
+    for i in range(1, len(poly)):
+        if poly[i] == 0:
+            poly_f.append(0)
+        else:
+            poly_f.append(poly[i]*i)
+    return poly_f
