1c860ee023cb4cf6d858e8777562bb0527afc713
armanruet <armanruet@gmail.com>
2026-06-03 15:40:04 +0000

    Task 17 done

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Derive happiness in oneself from a good day's work"""
+
+
+def poly_integral(poly, C=0):
+    """defining the function"""
+    poly_f = [C]
+    if not isinstance(poly, list) and not isinstance(C, int):
+        return None
+    if len(poly) == 1:
+        return poly
+    for i in range(0, len(poly)):
+        check = poly[i]/(i+1)
+        poly_f.append(int(check) if check.is_integer() else check)
+    if all(x == 0 for x in poly_f):
+        return [0]
+    return poly_f
