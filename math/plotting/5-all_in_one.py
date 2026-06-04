53a35fa7ed139369184b937637a1641ceb071a40
armanruet <armanruet@gmail.com>
2026-06-04 13:08:43 +0000

    Task 5 done

---
+++ math/plotting/5-all_in_one.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def all_in_one():
+
+    y0 = np.arange(0, 11) ** 3
+
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x1, y1 = np.random.multivariate_normal(mean, cov, 2000).T
+    y1 += 180
+
+    x2 = np.arange(0, 28651, 5730)
+    r2 = np.log(0.5)
+    t2 = 5730
+    y2 = np.exp((r2 / t2) * x2)
+
+    x3 = np.arange(0, 21000, 1000)
+    r3 = np.log(0.5)
+    t31 = 5730
+    t32 = 1600
+    y31 = np.exp((r3 / t31) * x3)
+    y32 = np.exp((r3 / t32) * x3)
+
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(10, 8))
+    # Top-left
+    ax1 = plt.subplot2grid((3, 2), (0, 0))
+    plt.xlim(0, 10)
+    plt.plot(y0, color='red')
+
+    # Top-right
+    ax2 = plt.subplot2grid((3, 2), (0, 1))
+    plt.xlabel('Height (in)')
+    plt.ylabel('Weight (lbs)')
+    plt.title("Men's Height vs Weight")
+    plt.scatter(x1, y1, color='magenta')
+
+    # Middle-left
+    ax3 = plt.subplot2grid((3, 2), (1, 0))
+    plt.xlabel("Time (years)")
+    plt.ylabel("Fraction Remaining")
+    plt.title("Exponential Decay of C-14")
+    plt.xlim(0, 28650)
+    plt.yscale("log")
+    plt.plot(x2, y2)
+
+    # Middle-right
+    ax4 = plt.subplot2grid((3, 2), (1, 1))
+    plt.xlabel("Time (years)")
+    plt.ylabel("Fraction Remaining")
+    plt.title("Exponential Decay of Radioactive Elements")
+    plt.xlim(0, 20000)
+    plt.ylim(0, 1)
+    plt.plot(x3, y31, linestyle='--', color="red", label="C-14")
+    plt.plot(x3, y32, linestyle='-', color="green", label="Ra-226")
+    plt.legend()
+
+    # Bottom row spans both columns
+    ax5 = plt.subplot2grid((3, 2), (2, 0), colspan=2)
+    plt.hist(student_grades, bins=np.arange(0, 101, 10), edgecolor='black')
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
+    plt.xticks(range(0, 101, 10))
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
+
+    plt.tight_layout()
+    plt.show()
