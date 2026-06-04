2134d91af0dead6eefc8ff1fa363d9ee59706bf7
armanruet <armanruet@gmail.com>
2026-06-04 09:40:21 +0000

    Task 4 not satished

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -13,5 +13,7 @@ def frequency():
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
-    plt.xticks(range(0, 110, 10))
+    plt.xticks(range(0, 101, 10))
+    plt.xlim(0,100)
+    plt.ylim(0,30)
     plt.show()
