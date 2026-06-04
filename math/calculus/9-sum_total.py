41e97881e1a01df28f6b81a203e54800ee0d2aac
armanruet <armanruet@gmail.com>
2026-06-04 14:03:07 +0000

    Task done while < 0

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ a function def summation_i_squared(n): that calculates"""
 
+
 def summation_i_squared(n):
     """defining the function"""
-    if not isinstance(n, int) or isinstance(n, bool):
+    if isinstance(n, bool) or not isinstance(n, int) or n < 0:
         return None
     else:
         return n * (n + 1) * (2 * n + 1) // 6
