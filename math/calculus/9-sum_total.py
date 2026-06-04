cc0b42d81f2861568ff86c91dc1caab9f5f7d2b4
armanruet <armanruet@gmail.com>
2026-06-04 14:26:23 +0000

    Task 9 with whole number

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,7 +4,7 @@
 
 def summation_i_squared(n):
     """defining the function"""
-    if type(n) is not int or n < 0:
+    if type(n) not in (int, float):
         return None
     else:
-        return n * (n + 1) * (2 * n + 1) // 6
+        return int(n * (n + 1) * (2 * n + 1) // 6)
