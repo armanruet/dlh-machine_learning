6f633a76ad622652a8aabe2d568e9b9f8ff3851b
armanruet <armanruet@gmail.com>
2026-06-04 13:37:49 +0000

    Task 6 done

---
+++ math/plotting/6-bars.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""defining the bar chart"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def bars():
+    """function"""
+    np.random.seed(5)
+    fruit = np.random.randint(0, 20, (4, 3))
+    plt.figure(figsize=(6.4, 4.8))
+    labels = ['Farrah', 'Fred', 'Felicia']
+    x = np.arange(len(labels))
+    width = 0.5
+
+    apples = fruit[0]
+    bananas = fruit[1]
+    oranges = fruit[2]
+    peaches = fruit[3]
+
+    plt.bar(x, apples, width=width, color='red', label='apples')
+    plt.bar(x, bananas, width=width, bottom=apples,
+            color='yellow', label='bananas')
+    plt.bar(x, oranges, width=width, bottom=apples +
+            bananas, color='#ff8000', label='oranges')
+    plt.bar(x, peaches, width=width, bottom=apples +
+            bananas + oranges, color='#ffe5b4', label='peaches')
+
+    plt.ylabel('Quantity of Fruit')
+    plt.ylim(0, 80)
+    plt.yticks(np.arange(0, 81, 10))
+    plt.xticks(x, labels)
+    plt.title('Number of Fruit per Person')
+    plt.legend()
