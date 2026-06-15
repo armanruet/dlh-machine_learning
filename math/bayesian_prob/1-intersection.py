ae874d3b453213f1e7210ca1ecaa1f25f2c91af5
armanruet <armanruet@gmail.com>
2026-06-15 15:02:33 +0000

    added pycodestyle

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -26,12 +26,14 @@ def likelihood(x, n, P):
     ncx = fact(n)/(fact(n-x)*fact(x))
     return ncx*(P**x)*((1-P)**(n-x))
 
+
 def intersection(x, n, P, Pr):
     """defining the function"""
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or P.ndim != 1:
@@ -45,4 +47,3 @@ def intersection(x, n, P, Pr):
     if not np.isclose(Pr.sum(), 1):
         raise ValueError("Pr must sum to 1")
     return likelihood(x, n, P) * Pr
-
