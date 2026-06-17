1461ad4d2f659bb2b588624d2dc6af6674c650a1
armanruet <armanruet@gmail.com>
2026-06-17 12:07:23 +0000

    Task 10 done

---
+++ math/probability/binomial.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""creating a Class"""
+
+
+class Binomial:
+    """defining the class"""
+
+    def __init__(self, data=None, n=1, p=0.5):
+        """initiating the constructor"""
+        if data is None:
+            if n <= 0:
+                raise ValueError("n must be a positive value")
+            if p <= 0 or p >= 1:
+                raise ValueError("p must be greater than 0 and less than 1")
+            self.n = int(n)
+            self.p = float(p)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+
+            mean = sum(data) / len(data)
+            variance = sum((x - mean) ** 2 for x in data) / len(data)
+
+            p = 1 - (variance / mean)
+            n = round(mean / p)
+            p = mean / n
+
+            self.n = n
+            self.p = p
