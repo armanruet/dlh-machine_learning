1e4942694a79533bc6d0eaadaca4c3a724a121c2
armanruet <armanruet@gmail.com>
2026-07-14 14:34:51 +0000

    Task 3

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-import pandas as pd
 """def"""
+import pandas as pd
 
 
 def rename(df):
     """defining the func"""
     df = df.rename(columns={"Timestamp": "Datetime"})
     df["Datetime"] = pd.to_datetime(df["Datetime"])
+    df = df[["Datetime", "Close"]]
     print(df[["Datetime", "Close"]])
     return df
