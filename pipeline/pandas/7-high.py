4f94dba9b5f5d9383fe08f8e561a660c72dde7fd
armanruet <armanruet@gmail.com>
2026-07-14 15:15:03 +0000

    Task 7

---
+++ pipeline/pandas/7-high.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Renames and reformats a DataFrame's Timestamp column."""
+
+
+def high(df):
+    """def func"""
+    return df = df.sort_values(by="High", ascending=False)
