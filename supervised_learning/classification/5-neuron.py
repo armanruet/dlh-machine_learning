9f0b91165862bf8fb43956164273b17ac6e331f1
armanruet <armanruet@gmail.com>
2026-08-08 23:09:14 +0200

    Classification Task: 5

---
+++ supervised_learning/classification/5-neuron.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""Neuron that performs binary classification"""
+
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """Initialize the neuron's attributes"""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.normal(0.0, 1.0, (1, nx))
+
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
+
+    def forward_prop(self, X):
+        """def FP"""
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """Cost calculate"""
+        m = Y.shape[1]
+        cost = -(1/m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        return cost
+
+    def evaluate(self, X, Y):
+        """evaluation"""
+        A = self.forward_prop(X)
+        predictions = np.where(A >= 0.5, 1, 0)
+        cost = self.cost(Y, A)
+        return predictions, cost
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """GD"""
+        m = X.shape[1]
+        dZ = A - Y
+        dW = (1 / m) * (dZ @ X.T)
+        db = (1 / m) * np.sum(dZ)
+
+        self.__W -= alpha * dW
+        self.__b -= alpha * db
