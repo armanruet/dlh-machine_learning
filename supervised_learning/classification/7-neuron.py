f21a368940ad65e4adee39e6c5a0a3ee014877cb
armanruet <armanruet@gmail.com>
2026-08-09 17:25:21 +0200

    Classification Task: 7

---
+++ supervised_learning/classification/7-neuron.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+"""Neuron that performs binary classification"""
+
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """Initialize the neuron's attributes"""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.normal(0.0, 1.0, (1, nx))
+
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
+
+    def forward_prop(self, X):
+        """def FP"""
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """Cost calculate"""
+        m = Y.shape[1]
+        cost = -(1/m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        return cost
+
+    def evaluate(self, X, Y):
+        """evaluation"""
+        A = self.forward_prop(X)
+        predictions = np.where(A >= 0.5, 1, 0)
+        cost = self.cost(Y, A)
+        return predictions, cost
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """GD calculation"""
+        m = X.shape[1]
+        dZ = A - Y
+        dW = (1 / m) * (dZ @ X.T)
+        db = (1 / m) * np.sum(dZ)
+
+        self.__W -= alpha * dW
+        self.__b -= alpha * db
+
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        """Training Network"""
+        if type(iterations) is not int:
+            raise TypeError("iterations must be an integer")
+        if iterations < 1:
+            raise ValueError("iterations must be a positive integer")
+        if type(alpha) is not float:
+            raise TypeError("alpha must be a float")
+        if alpha <= 0:
+            raise ValueError("alpha must be positive")
+        for _ in range(iterations):
+            A = self.forward_prop(X)
+            self.gradient_descent(X, Y, A, alpha)
+        return self.evaluate(X, Y)
+
+    def train(self, X, Y, iterations=5000, alpha=0.05,
+              verbose=True, graph=True, step=100):
+        if type(iterations) is not int:
+            raise TypeError("iterations must be an integer")
+        if type(alpha) is not float:
+            raise TypeError("alpha must be a float")
+        if verbose or graph:
+            if type(step) is not int:
+                raise TypeError("step must be an integer")
+            if step <= 0 or step > iterations:
+                raise ValueError("step must be positive and <= iterations")
+
+        costs = []
+        for i in range(iterations+1):
+            # make predictions from X
+            A = self.forward_prop(X)
+            # improve weight (W) and bias (b) from predictions A
+            if i <= iterations:
+                self.gradient_descent(X, Y, A, alpha)
+            # update loop and report cost
+            cost = self.cost(Y, A)
+            costs.append(cost)
+            if verbose is True:
+                if i % step == 0 or i == iterations:
+                    print(f"Cost after {i} iterations: {cost}")
+        # plot cost graph
+        if graph is True:
+            plt.plot(costs)
+            plt.xlabel('iteration')
+            plt.ylabel('cost')
+            plt.title('Training Cost')
+            plt.show()
+        return self.evaluate(X, Y)
