852000c87a9a1b63e76a3f31f006aa4ddf2e35c6
armanruet <armanruet@gmail.com>
2026-09-01 19:30:52 +0200

    Optimization: Task 0

---
+++ supervised_learning/optimization/0-norm_constants.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Normalization Constants"""
+import numpy as np
+
+
+def normalization_constants(X):
+    """Def the function"""
+    mean = np.mean(X, axis=0)
+    std = np.std(X, axis=0)
+
+    return mean, std
