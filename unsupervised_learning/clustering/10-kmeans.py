13305f301969aa545048c6925291334bb17acaf2
armanruet <armanruet@gmail.com>
2026-09-04 10:46:20 +0200

    Clustering Tasks 10 corrected

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
-"""kmeans in Dataset"""
-import sklearn.cluster
+""" Module for K-means clustering on the dataset X. """
+import sklearn.cluster as skc
 
 
 def kmeans(X, k):
-    """def the func"""
-    model = sklearn.cluster.KMeans(n_clusters=k, random_state=0, n_init="auto")
-    clss = model.fit_predict(X)
-    C = model.cluster_centers_
-    return C, clss
+    """ Performs K-means clustering on the dataset X.
+
+    Args:
+        X : shape (n, d) containing the dataset.
+        k : number of clusters.
+
+    Returns:
+        C : shape (k, d) containing centroid means.
+        clss : shape (n,) containing cluster indices for each point.
+    """
+    model = skc.KMeans(n_clusters=k).fit(X)
+    return model.cluster_centers_, model.labels_
