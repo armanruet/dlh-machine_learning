f0cea406643e5c600690e70367c30d235a79168f
armanruet <armanruet@gmail.com>
2026-09-06 22:35:23 +0200

    Opt task 10 corrected

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -5,9 +5,7 @@ Adam Upgraded
 import tensorflow as tf
 
 
-def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+def create_Adam_op(alpha, beta1, beta2, epsilon):
     """function that implements Adam gradient descent in tensorflow"""
-    return tf.train.AdamOptimizer(
-        learning_rate=alpha, beta1=beta1, beta2=beta2, epsilon=epsilon,
-        use_locking=False, name='Adam'
-    ).minimize(loss)
+    return tf.keras.optimizers.Adam(
+        learning_rate=alpha, beta_1=beta1, beta_2=beta2, epsilon=epsilon)
