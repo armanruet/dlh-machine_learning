eda85a7d21c12945e935d322dbb4c5f92b6ef98b
armanruet <armanruet@gmail.com>
2026-09-16 23:24:31 +0200

    Data_Aug Task 0 fixed

--- pipeline/data_augmentation/0-flip.py
+++ pipeline/data_augmentation/0-flip.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""Flip"""
+"""Module for flipping an image horizontally using TensorFlow"""
 import tensorflow as tf
 
 
 def flip_image(image):
-    """def the func"""
+    """def the func to flip image"""
     return tf.image.flip_left_right(image)
